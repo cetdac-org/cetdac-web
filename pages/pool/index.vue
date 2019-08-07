a2a7298330ea51a418a0bc617efdb5034b6546a6
AlanGuo <AlanGuo>
2019-08-07 21:25:37 +0800

    update

---
+++ pages/pool/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="cetdac-web-pool">
+  </div>
+</template>
+<script>
+import Vue from "vue"
+import { mapState } from "vuex"
+
+export default {
+  head() {
+    return {
+      title: 'CETDAC',
+    }
+  },
+
+  computed:{
+    ...mapState(["lang"]),
+  },
+  data () {
+    return {
+    }
+  },
+
+  mounted(){
+  
+  },
+
+  methods:{
+    buy: function(){
+
+    }
+  },
+  
+  beforeDestroy(){
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.cetdac-web-pool {
+  max-width: 1200px;
+  margin:0 auto;
+}
+</style>
+
